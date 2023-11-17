ddd54c576f18f3f871b9c0361b9d75830c544e09
HyunJoo-J <jhj2844@naver.com>
2023-11-17 12:12:03 +0900

    add attraction

---
+++ vue/src/components/attraction/AttractionMap.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { onMounted } from "vue";
+
+const { VITE_KAKAO_JAVASCRIPT_APP_KEY } = import.meta.env;
+
+var map;
+
+onMounted(() => {
+  if (window.kakao && window.kakao.maps) {
+    initMap();
+  } else {
+    const script = document.createElement("script");
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${
+      import.meta.env.VITE_KAKAO_JAVASCRIPT_APP_KEY
+    }&libraries=services,clusterer`;
+    /* global kakao */
+    script.onload = () => kakao.maps.load(() => initMap());
+    document.head.appendChild(script);
+  }
+});
+
+const initMap = () => {
+  const container = document.getElementById("map");
+  const options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3,
+  };
+  map = new kakao.maps.Map(container, options);
+
+  // loadMarkers();
+};
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
